3944c8f15eb2bc221d08f51d8b7ddeaf1bda0f6b
Glenn Salter <72780073+gsalter-cityu@users.noreply.github.com>
2020-11-25 23:06:21 +0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,104 +1,82 @@
-
-isOver = false;
-isFirst = true;
-isStart = false;
+var isOver = false;
+var isFirst = true;
+var isStart = false;
 var firstPlayer;
 textBox = document.querySelector("input[type='text']");
-
 function occupyBox(obj) {
-    // This function occupies a box with either X or O depending on number of emtpy boxes
     boxes = document.querySelectorAll(".box");
     empty_boxes = 0;
     for (let i=0; i<9; i++) {
-        if (boxes[i].innerHTML === " ") {
-            empty_boxes += 1
-        }
+        if (boxes[i].innerHTML === " ") {empty_boxes += 1}
     }
     if (empty_boxes%2 === 1) {
-        obj.innerHTML = "❌";
-        obj.value = "x";
-    } else {
-        obj.innerHTML = "⭕";
-        obj.value = "o";
+        if (obj.innerHTML === " ") {obj.innerHTML = "❌";}
+    }
+    else {
+        if (obj.innerHTML === " ") {obj.innerHTML = "⭕";}
     }
 }
-
 function updateBoard() {
-    // This function updates the gameBoard
     boxes = document.querySelectorAll(".box");
     for (let i=0; i<9; i++) {
         var value
-        if (boxes[i].innerHTML === "❌") {
-            value = "x"
+        if (firstPlayer===1) {
+            if (boxes[i].innerHTML === "❌") {value = "x"}
+            if (boxes[i].innerHTML === "⭕") {value = "o"}
         }
-        if (boxes[i].innerHTML === "⭕") {
-            value = "o"
+        if (firstPlayer===0) {
+            if (boxes[i].innerHTML === "❌") {value = "o"}
+            if (boxes[i].innerHTML === "⭕") {value = "x"}
         }
-        if (boxes[i].innerHTML === " ") {
-            value = "_"
-        }
+        if (boxes[i].innerHTML === " ") {value = "_"}
         board[parseInt(boxes[i].id[1])-1][parseInt(boxes[i].id[2])-1] = value;
     }
 }
-
 function resetBoard() {
     boxes = document.querySelectorAll(".box");
     second = document.querySelector("#second");
     first = document.querySelector("#first");
-
-    for (let i=0; i<9; i++) {
-        boxes[i].innerHTML = " ";
-    }
+    for (let i=0; i<9; i++) {boxes[i].innerHTML = " ";}
+    board = [['_','_','_'], ['_','_','_'], ['_','_','_']];
     isOver = false;
     isFirst = true;
     isStart = false;
     textBox.value = "Pick ❌ or ⭕";
 }
-
+function displayMessage(msg1, msg2="",time=1000) {
+    textBox.value = msg1;
+    setTimeout(function () {textBox.value = msg2;},time)
+}
 function isGameOver() {
     score = evaluate(board);
-
+    console.log(score);
     if (score===10 || score ===-10) {
         isOver = true;
-        board = [
-    ['_','_','_'],
-    ['_','_','_'],
-    ['_','_','_']
-];
         if (score===10) {
             displayMessage("AI Wins! Good Luck Next Time", "Pick ❌ or ⭕", 4000);
+            alert("AI Wins");
         }
         if (score===-10) {
             displayMessage("Congratulations, You Win!", "Pick ❌ or ⭕", 4000);
+            alert("You Win");
         }
         setTimeout(resetBoard, 1000);
     }
     if (!isMovesLeft(board)) {
         isOver = true;
-        board = [
-    ['_','_','_'],
-    ['_','_','_'],
-    ['_','_','_']
-];
-        displayMessage("Draw! Nobody Won.", "Pick ❌ or ⭕", 4000)
+        displayMessage("Draw! Nobody Won.", "Pick ❌ or ⭕", 4000);
+        alert("Draw!");
         setTimeout(resetBoard, 1000);
     }
 }
-
 function makeMove(obj) {
-
-    if (isMovesLeft(board) && !isOver && isStart) {
+    if (!isOver && isStart) {
         occupyBox(obj);
         updateBoard();
-
-        // Gets number of empty boxes
         empty_boxes = 0;
         for (let i = 0; i < 9; i++) {
-            if (boxes[i].innerHTML === " ") {
-                empty_boxes += 1
-            }
+            if (boxes[i].innerHTML === " ") {empty_boxes += 1}
         }
-        // Computer makes move if the number of empty boxes are odd
         if (empty_boxes % 2 === firstPlayer && empty_boxes !== 0) {
             displayMessage("AI Made a Move...", "Your Move...")
             bestMove = findBestMove();
@@ -107,36 +85,25 @@ function makeMove(obj) {
             occupyBox(box);
             updateBoard();
         }
-
     }
-
     isGameOver();
 }
-
 function firstMove(obj) {
     textBox = document.querySelector("input[type='text']");
     isStart = true;
     if (isFirst) {
         if (obj.innerHTML === "⭕") {
-
+            firstPlayer = 1;
             bestMove = findBestMove();
             div_id = "_" + String(bestMove.row + 1) + String(bestMove.col + 1);
             box = document.getElementById(div_id);
-            firstPlayer = 1;
             makeMove(box);
-            displayMessage("You chose ⭕, go second.","Your Move...")
+            displayMessage("You chose ⭕, go second.","Your Move...");
         }
         if (obj.innerHTML === "❌") {
             firstPlayer = 0;
-            displayMessage("You chose ❌, go first. AI is waiting...","Your Move...",2000)
+            displayMessage("You chose ❌, go first. AI is waiting...","Your Move...",2000);
         }
         isFirst = false;
     }
-}
-
-function displayMessage(msg1, msg2="",time=1000) {
-    textBox.value = msg1;
-    setTimeout(function () {
-        textBox.value = msg2;
-    },time)
 }
--- tictactoe.js
+++ tictactoe.js
@@ -1,84 +1,40 @@
-board = [
-    ['_','_','_'],
-    ['_','_','_'],
-    ['_','_','_']
-];
-
-class Move {
-    constructor(row, col) {
-        this.row = row;
-        this.col = col;
-    }
-}
-
-function displayBoard(b) {
-    for (let row=0; row<3; row++) {
-        console.log(board[row])
-    }
-    console.log('\n');
-}
-
+board = [['x','_','_'], ['_','_','_'], ['_','_','_']];
+class Move {constructor(row, col) {this.row = row;this.col = col;}}
 function isMovesLeft(b) {
     for (let i=0; i<3; i++) {
         for (let j=0; j<3; j++) {
-            if (b[i][j] === '_') {
-                return true;
-            }
+            if (b[i][j] === '_') {return true;}
         }
     }
     return false;
 }
-
 function evaluate(b) {
     for (let row=0; row<3; row++) {
         if (b[row][0] === b[row][1] && b[row][1] === b[row][2]) {
-            if (b[row][0] === 'x') {
-                return 10;
-            }
-            if (b[row][0] === 'o') {
-                return -10;
-            }
+            if (b[row][0] === 'x') {return 10;}
+            if (b[row][0] === 'o') {return -10;}
         }
     }
     for (let col=0; col<3; col++) {
         if (b[0][col] === b[1][col] && b[1][col] === b[2][col]) {
-            if (b[0][col] === 'x') {
-                return 10;
-            }
-            if (b[0][col] === 'o') {
-                return -10;
-            }
+            if (b[0][col] === 'x') {return 10;}
+            if (b[0][col] === 'o') {return -10;}
         }
     }
     if (b[0][0] === b[1][1] && b[1][1] === b[2][2]) {
-        if (b[0][0] === 'x') {
-            return 10;
-        }
-        if (b[0][0] === 'o') {
-            return -10;
-        }
+        if (b[0][0] === 'x') {return 10;}
+        if (b[0][0] === 'o') {return -10;}
     }
     if (b[0][2] === b[1][1] && b[1][1] === b[2][0]) {
-        if (b[0][2] === 'x') {
-            return 10;
-        }
-        if (b[0][2] === 'o') {
-            return -10;
-        }
+        if (b[0][2] === 'x') {return 10;}
+        if (b[0][2] === 'o') {return -10;}
     }
     return 0;
 }
-
 function minimax(alpha, beta ,depth=1, turn='o') {
-
     let score = evaluate(board);
-    if (score === 10 || score === -10) {
-        return score-depth;
-    }
-    if (isMovesLeft(board) === false) {
-        return 0-depth;
-    }
-
+    if (score === 10 || score === -10) {return score-depth;}
+    if (isMovesLeft(board) === false) {return 0-depth;}
     if (turn === 'x') {
         let bestAI = -1000;
         for (let row=0; row<3; row++) {
@@ -89,16 +45,12 @@ function minimax(alpha, beta ,depth=1, turn='o') {
                     board[row][col] = '_';
                     bestAI = Math.max(val, bestAI);
                     alpha = Math.max(alpha, bestAI);
-                    if (beta <= alpha) {
-                        break;
-                    }
+                    if (beta <= alpha) {break;}
                 }
             }
         }
-
         return bestAI;
     }
-
     if (turn === 'o') {
         let bestHuman = Infinity;
         for (let row=0; row<3; row++) {
@@ -109,16 +61,13 @@ function minimax(alpha, beta ,depth=1, turn='o') {
                     board[row][col] = '_';
                     bestHuman = Math.min(val, bestHuman);
                     beta = Math.min(beta, bestHuman);
-                    if (beta <= alpha) {
-                        break;
-                    }
+                    if (beta <= alpha) {break;}
                 }
             }
         }
         return bestHuman;
     }
 }
-
 function findBestMove() {
     let bestMove = new Move(-1,-1);
     let bestScore = -Infinity;
@@ -127,7 +76,6 @@ function findBestMove() {
             if (board[row][col] === '_') {
                 board[row][col] = 'x';
                 let temp = minimax(-Infinity, Infinity);
-
                 board[row][col] = '_';
                 if (temp > bestScore) {
                     bestMove.row = row;
